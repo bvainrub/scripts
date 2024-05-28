f8d2d174a4719616f21ad3206c1f085c8223d90f
bvainrub <bvainrub@gmail.com>
2024-05-28 13:49:24 -0300

    Add a simple network connectivity check

--- all_checks.py
+++ all_checks.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import sys
+import socket
+import psutil
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
+
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise."""    
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
 
 def main():
     if check_reboot():

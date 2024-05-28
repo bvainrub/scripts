c07d299b6f73f6fbead62b616a45c38b803232a7
bvainrub <bvainrub@gmail.com>
2024-05-27 23:43:04 -0300

    Create wrapper function for check_disk_full

--- all_checks.py
+++ all_checks.py
@@ -9,7 +9,7 @@ def check_reboot():
 
 def main():
     if check_reboot():
-        print("Pending Reboot.")
+        print("Pending Reboot.Step 3 new branch")
         sys.exit(1)
     print("Everything OK. ")
     sys.exit(0)

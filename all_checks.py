e00d4e9f017d8576bbf4a63c1361cbd6b94a5ee1
bvainrub <bvainrub@gmail.com>
2024-05-27 23:09:53 -0300

    Rename min_absolute to min_gb, use parameter names

--- all_checks.py
+++ all_checks.py
@@ -9,7 +9,7 @@ def check_reboot():
 
 def main():
     if check_reboot():
-        print("Pending Reboot.")
+        print("Pending Reboot. Begin reboot")
         sys.exit(1)
     print("Everything OK. ")
     sys.exit(0)
